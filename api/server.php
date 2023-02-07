6208bbf0cf5b488085607f1c844eaefbd3cf0d02
davi <a2106897@institutmontilivi.cat>
2023-02-07 19:38:22 +0100

    inicio Api

---
+++ api/server.php
@@ -0,0 +1,25 @@
+<?php
+//agregar el objeto bdd
+include  './BdD.php';
+class server{
+    //separamos el url en partes 
+    if(!isset($_COOKIE['token']))
+        {
+            //si no viene del login lo redirigimos al login
+            //si viene del login le damos una cooki token con la funcion
+        }
+    else{
+        //un if con una funcion que mira si el token existe si error
+            //una vez dentro mira la direccion en la que se encustra y el metodo que quiere utilizar
+                //dentro de las opciones estan la funciones pertinentes
+
+    }
+
+
+
+    //creamos el objeto base de datos
+
+    $bdd=new Bdd()
+    
+}
+?>
