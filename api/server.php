ea7f3273bdfbbc522ba1b19b65ac19b71797b75e
davi <a2106897@institutmontilivi.cat>
2023-02-10 19:22:52 +0100

    server parte 2

--- api/server.php
+++ api/server.php
@@ -1,6 +1,6 @@
 <?php
 //agregar el objeto bdd
-include  './BdD.php';
+include  './Bdd.php';
 class server{
     //separamos el url en partes 
     if(!isset($_COOKIE['token']))
@@ -9,7 +9,7 @@ class server{
             //si viene del login le damos una cooki token con la funcion
         }
     else{
-        //un if con una funcion que mira si el token existe si error
+        //un if con una funcion que mira si el token existe si no error
             //una vez dentro mira la direccion en la que se encustra y el metodo que quiere utilizar
                 //dentro de las opciones estan la funciones pertinentes
 
